171026773e42275cd69c0f05ae4bd35f04606753
haikaljs <haikaljavascript@gmail.com>
2023-09-28 18:30:57 +0800

    Create new button inside Surveys page

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,5 +1,7 @@
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import PageComponents from "../components/PageComponents";
 import SurveyListItem from "../components/SurveyListItem";
+import TButton from "../components/core/TButton";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Surveys = () => {
@@ -8,7 +10,15 @@ const Surveys = () => {
         console.log("On delete click");
     };
     return (
-        <PageComponents title="Surveys">
+        <PageComponents
+            title="Surveys"
+            buttons={
+                <TButton color="green" to="/surveys/create">
+                    <PlusCircleIcon className="h-6 w-6 mr-2" />
+                    Create new
+                </TButton>
+            }
+        >
             <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
                 {surveys.map((survey) => (
                     <SurveyListItem
