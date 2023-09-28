c07c9d1b88af5f8d9ab5c438489ba5b4afc31a21
haikaljs <haikaljavascript@gmail.com>
2023-09-28 18:27:29 +0800

    SurveyView

---
+++ react/src/views/SurveyView.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PageComponents from "../components/PageComponents";
+
+const SurveyView = () => {
+    return (
+        <PageComponents title="Create new survey">Survey form</PageComponents>
+    );
+};
+
+export default SurveyView;
